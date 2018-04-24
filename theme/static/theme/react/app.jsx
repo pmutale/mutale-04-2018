00900bb808b17a69d236f2b6fa3510c691d9e0fb
P. Mutale <pmutale@vacansoleil.com>
2018-04-24 17:14:45 +0200

    Theme Font and Colours

--- theme/static/theme/react/app.jsx
+++ theme/static/theme/react/app.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import "../../../scss/main";
 import { hot } from 'react-hot-loader'
-import Header from './components/header/header';
+import Base from './components/base'
+import ErrorBoundary from './components/error_boundary/error_boundary'
 
 
-const renderApp = (Component) => {
-    ReactDOM.render(
-        <Component/>,
-        document.getElementById('root'),
-    )
+const renderApp = (ErrorBoundary) => {
+  ReactDOM.render(
+    <ErrorBoundary>
+      <Base/>
+    </ErrorBoundary>,
+    document.getElementById('root'),
+  )
 };
-hot(module)(renderApp(Header));
+hot(module)(renderApp(ErrorBoundary));
